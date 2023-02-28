83c43c4f7604944af1a61a5e5b0f86b66f6c68f1
dzagar88 <dzagarit@gmail.com>
2023-02-28 00:00:17 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//write your code here
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+
+for (let proN of pronoun) {
+  for (let add of adj) {
+    for (let nou of noun) {
+      console.log(`${proN}${add}${nou}.com`);
+    }
+  }
+}
+// };
